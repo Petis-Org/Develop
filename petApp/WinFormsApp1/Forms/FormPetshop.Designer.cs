be4a6ee3793c3edfd2aff63b300929caaa34bb4d
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2023-05-19 10:17:19 -0300

    att

--- petApp/WinFormsApp1/Forms/FormPetshop.Designer.cs
+++ petApp/WinFormsApp1/Forms/FormPetshop.Designer.cs
@@ -35,18 +35,17 @@
             label4 = new Label();
             label3 = new Label();
             label1 = new Label();
-            gridPets = new DataGridView();
             button3 = new Button();
             button2 = new Button();
             button1 = new Button();
             comboBox2 = new ComboBox();
             comboBox1 = new ComboBox();
-            Column1 = new DataGridViewTextBoxColumn();
-            Column2 = new DataGridViewTextBoxColumn();
-            Column3 = new DataGridViewTextBoxColumn();
-            Column4 = new DataGridViewTextBoxColumn();
-            Column5 = new DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)gridPets).BeginInit();
+            dataGridView1 = new DataGridView();
+            dataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn2 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn3 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn4 = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // label4
@@ -54,7 +53,7 @@
             label4.Anchor = AnchorStyles.None;
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
-            label4.Location = new Point(253, 22);
+            label4.Location = new Point(195, 9);
             label4.Name = "label4";
             label4.Size = new Size(200, 28);
             label4.TabIndex = 23;
@@ -65,7 +64,7 @@
             label3.Anchor = AnchorStyles.None;
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(50, 133);
+            label3.Location = new Point(7, 120);
             label3.Name = "label3";
             label3.Size = new Size(58, 21);
             label3.TabIndex = 18;
@@ -76,61 +75,17 @@
             label1.Anchor = AnchorStyles.None;
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(52, 104);
+            label1.Location = new Point(9, 91);
             label1.Name = "label1";
             label1.Size = new Size(56, 21);
             label1.TabIndex = 17;
             label1.Text = "Estado";
             // 
-            // gridPets
-            // 
-            gridPets.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            gridPets.BackgroundColor = SystemColors.InactiveCaption;
-            gridPets.BorderStyle = BorderStyle.None;
-            gridPets.CellBorderStyle = DataGridViewCellBorderStyle.RaisedHorizontal;
-            gridPets.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = Color.RoyalBlue;
-            dataGridViewCellStyle1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            dataGridViewCellStyle1.ForeColor = SystemColors.Window;
-            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
-            gridPets.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            gridPets.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            gridPets.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5 });
-            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = SystemColors.Window;
-            dataGridViewCellStyle3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            dataGridViewCellStyle3.ForeColor = SystemColors.ControlText;
-            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
-            gridPets.DefaultCellStyle = dataGridViewCellStyle3;
-            gridPets.EnableHeadersVisualStyles = false;
-            gridPets.GridColor = SystemColors.ControlDarkDark;
-            gridPets.Location = new Point(252, 53);
-            gridPets.Name = "gridPets";
-            gridPets.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = SystemColors.ActiveCaptionText;
-            dataGridViewCellStyle4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            dataGridViewCellStyle4.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle4.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
-            gridPets.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
-            gridPets.RowHeadersVisible = false;
-            gridPets.RowTemplate.Height = 25;
-            gridPets.Size = new Size(536, 320);
-            gridPets.TabIndex = 16;
-            gridPets.CellContentClick += gridPets_CellContentClick;
-            // 
             // button3
             // 
             button3.Anchor = AnchorStyles.None;
             button3.FlatStyle = FlatStyle.Flat;
-            button3.Location = new Point(659, 379);
+            button3.Location = new Point(660, 389);
             button3.Name = "button3";
             button3.Size = new Size(129, 49);
             button3.TabIndex = 15;
@@ -141,7 +96,7 @@
             // 
             button2.Anchor = AnchorStyles.None;
             button2.FlatStyle = FlatStyle.Flat;
-            button2.Location = new Point(457, 379);
+            button2.Location = new Point(441, 389);
             button2.Name = "button2";
             button2.Size = new Size(129, 49);
             button2.TabIndex = 14;
@@ -153,7 +108,7 @@
             // 
             button1.Anchor = AnchorStyles.None;
             button1.FlatStyle = FlatStyle.Flat;
-            button1.Location = new Point(252, 379);
+            button1.Location = new Point(195, 389);
             button1.Name = "button1";
             button1.Size = new Size(129, 49);
             button1.TabIndex = 13;
@@ -165,7 +120,7 @@
             // 
             comboBox2.Anchor = AnchorStyles.None;
             comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(114, 133);
+            comboBox2.Location = new Point(71, 120);
             comboBox2.Name = "comboBox2";
             comboBox2.Size = new Size(118, 23);
             comboBox2.TabIndex = 25;
@@ -178,56 +133,95 @@
             comboBox1.FormattingEnabled = true;
             comboBox1.IntegralHeight = false;
             comboBox1.Items.AddRange(new object[] { "AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MG", "MS", "MT", "PA", "PB", "PE", "PI", "PR", "RJ", "RN", "RO", "RR", "RS", "SC", "SE", "SP", "TO" });
-            comboBox1.Location = new Point(114, 104);
+            comboBox1.Location = new Point(71, 91);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(118, 23);
             comboBox1.TabIndex = 24;
             // 
-            // Column1
+            // dataGridView1
+            // 
+            dataGridView1.Anchor = AnchorStyles.None;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.BackgroundColor = SystemColors.InactiveCaption;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.RaisedHorizontal;
+            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = Color.RoyalBlue;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.Window;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn1, dataGridViewTextBoxColumn2, dataGridViewTextBoxColumn3, dataGridViewTextBoxColumn4 });
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = SystemColors.Window;
+            dataGridViewCellStyle3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
+            dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.GridColor = SystemColors.ControlDarkDark;
+            dataGridView1.Location = new Point(195, 40);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = SystemColors.ActiveCaptionText;
+            dataGridViewCellStyle4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
+            dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(594, 343);
+            dataGridView1.TabIndex = 27;
+            // 
+            // dataGridViewTextBoxColumn1
             // 
             dataGridViewCellStyle2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            Column1.DefaultCellStyle = dataGridViewCellStyle2;
-            Column1.HeaderText = "Petshop";
-            Column1.Name = "Column1";
+            dataGridViewTextBoxColumn1.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewTextBoxColumn1.HeaderText = "Petshop";
+            dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
             // 
-            // Column2
+            // dataGridViewTextBoxColumn2
             // 
-            Column2.HeaderText = "";
-            Column2.Name = "Column2";
+            dataGridViewTextBoxColumn2.HeaderText = "Avaliação";
+            dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
             // 
-            // Column3
+            // dataGridViewTextBoxColumn3
             // 
-            Column3.HeaderText = "Column3";
-            Column3.Name = "Column3";
+            dataGridViewTextBoxColumn3.HeaderText = "Telefone";
+            dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
             // 
-            // Column4
+            // dataGridViewTextBoxColumn4
             // 
-            Column4.HeaderText = "Column4";
-            Column4.Name = "Column4";
-            // 
-            // Column5
-            // 
-            Column5.HeaderText = "Column5";
-            Column5.Name = "Column5";
+            dataGridViewTextBoxColumn4.HeaderText = "Endereço";
+            dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
             // 
             // FormPetshop
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
             Controls.Add(comboBox2);
             Controls.Add(comboBox1);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label1);
-            Controls.Add(gridPets);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Name = "FormPetshop";
             Text = "FormPetshop";
             Load += FormPetshop_Load_1;
-            ((System.ComponentModel.ISupportInitialize)gridPets).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -237,16 +231,15 @@
         private Label label4;
         private Label label3;
         private Label label1;
-        private DataGridView gridPets;
         private Button button3;
         private Button button2;
         private Button button1;
         private ComboBox comboBox2;
         private ComboBox comboBox1;
-        private DataGridViewTextBoxColumn Column1;
-        private DataGridViewTextBoxColumn Column2;
-        private DataGridViewTextBoxColumn Column3;
-        private DataGridViewTextBoxColumn Column4;
-        private DataGridViewTextBoxColumn Column5;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
     }
 }
