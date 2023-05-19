ed9770e9f579940c03d8e58572f844e2f8bd1d90
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2023-05-19 09:01:01 -0300

    att

--- petApp/WinFormsApp1/Forms/FormPetshop.Designer.cs
+++ petApp/WinFormsApp1/Forms/FormPetshop.Designer.cs
@@ -28,6 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             label4 = new Label();
             label3 = new Label();
             label1 = new Label();
@@ -37,6 +41,11 @@
             button1 = new Button();
             comboBox2 = new ComboBox();
             comboBox1 = new ComboBox();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)gridPets).BeginInit();
             SuspendLayout();
             // 
@@ -75,10 +84,43 @@
             // 
             // gridPets
             // 
-            gridPets.Anchor = AnchorStyles.None;
+            gridPets.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            gridPets.BackgroundColor = SystemColors.InactiveCaption;
+            gridPets.BorderStyle = BorderStyle.None;
+            gridPets.CellBorderStyle = DataGridViewCellBorderStyle.RaisedHorizontal;
+            gridPets.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = Color.RoyalBlue;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.Window;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            gridPets.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             gridPets.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gridPets.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5 });
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = SystemColors.Window;
+            dataGridViewCellStyle3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
+            gridPets.DefaultCellStyle = dataGridViewCellStyle3;
+            gridPets.EnableHeadersVisualStyles = false;
+            gridPets.GridColor = SystemColors.ControlDarkDark;
             gridPets.Location = new Point(252, 53);
             gridPets.Name = "gridPets";
+            gridPets.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = SystemColors.ActiveCaptionText;
+            dataGridViewCellStyle4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
+            gridPets.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            gridPets.RowHeadersVisible = false;
             gridPets.RowTemplate.Height = 25;
             gridPets.Size = new Size(536, 320);
             gridPets.TabIndex = 16;
@@ -141,6 +183,33 @@
             comboBox1.Size = new Size(118, 23);
             comboBox1.TabIndex = 24;
             // 
+            // Column1
+            // 
+            dataGridViewCellStyle2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            Column1.DefaultCellStyle = dataGridViewCellStyle2;
+            Column1.HeaderText = "Petshop";
+            Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "";
+            Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Column3";
+            Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "Column4";
+            Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            Column5.HeaderText = "Column5";
+            Column5.Name = "Column5";
+            // 
             // FormPetshop
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -174,5 +243,10 @@
         private Button button1;
         private ComboBox comboBox2;
         private ComboBox comboBox1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
     }
 }
