ae9340121c3a4dc715319f7603bdeeefb6e6bb72
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2023-05-24 09:32:34 -0300

    att db connection

---
+++ petApp/dbClassLibra/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace dbClassLibra
+{
+    public class Class1
+    {
+        private static void CreateCommand(string queryString,
+            string connectionString){
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                object value = command.Connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
