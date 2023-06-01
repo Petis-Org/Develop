0202fd4fd8ff8e0201d04535d03ed7e8149601e3
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2023-06-01 10:55:52 -0300

    att

--- petApp/WinFormsApp1/Forms/FormsSecundar/controlVetRequest.cs
+++ petApp/WinFormsApp1/Forms/FormsSecundar/controlVetRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dbClassLibra;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,13 @@ namespace WinFormsApp1.Forms.FormsSecundar
 {
     public partial class controlVetRequest : Form
     {
+        public List<vetRequest> listVetReq()
+        {
+            string querySelect = "SELECT * FROM VETREQUEST";
+            var connection = new petsCad();
+            var list = connection.SelectCommandVetReq(querySelect);
+            return list;
+        }
         public controlVetRequest()
         {
             InitializeComponent();
